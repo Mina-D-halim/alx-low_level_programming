65ab22e13051d5b8febfc30abcd7b47a73700640
Mina-D-halim <mina.dawoud@onewaygroups.com>
2024-02-12 07:22:27 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include<main.h>
+/**
+ * _puts - prints a string
+ * @str: poiner
+ */
+void _puts(char *st)
+{
+	int i;
+
+	for (i = 0; st[i] != '\0'; i++)
+	{
+		_putchar(st[i]);
+	}
+	_putchar('\n');
+}    	 
