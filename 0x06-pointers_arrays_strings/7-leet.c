5930005cb4643bf0b6b988eb9ed339492cfb2d55
Mina-D-halim <mina.dawoud@onewaygroups.com>
2024-02-24 06:59:09 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - change vowels to numbers.
+ * @s: analized string.
+ *
+ * Return: String with all vowels changed.
+ */
+char *leet(char *s)
+{
+	char ch[] = "aeotlAEOTL";
+	char nu[] = "4307143071";
+	int i = 0;
+	int j;
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (*(s + i) == ch[j])
+			{
+				*(s + i) = nu[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
